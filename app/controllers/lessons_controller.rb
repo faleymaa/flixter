b4187fbd24ae5de4383f0acf011860eff0738506
Megan Faley <megan.faley@gmail.com>
2019-08-06 01:15:36 +0000

    lesson 28 adding permissons for course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,14 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrolled_in_current_course
+
 	def show
+	end
+
+	def require_enrolled_in_current_course
+		if current_user.enrolled_in?(current_lesson.section.course) != true
+			redirect_to course_url, alert: 'You must be enrolled in this course.'
+		end
 	end
 
 	private
