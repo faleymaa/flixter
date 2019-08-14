82f4b7109acb1b218e760a33f116073e93adee13
Megan Faley <megan.faley@gmail.com>
2019-08-14 17:32:33 +0000

    fixed courses bug

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,7 +7,7 @@ class LessonsController < ApplicationController
 
 	def require_enrolled_in_current_course
 		if current_user.enrolled_in?(current_lesson.section.course) != true
-			redirect_to course_url, alert: 'You must be enrolled in this course.'
+			redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in this course.'
 		end
 	end
 
--- db/migrate/20190724015104_create_courses.rb
+++ db/migrate/20190724015104_create_courses.rb
@@ -2,10 +2,10 @@ class CreateCourses < ActiveRecord::Migration[5.2]
   def change
     create_table :courses do |t|
   		t.string :title
-      	t.text :description
-     	 t.decimal :cost
-      	t.integer :user_id
-      	t.timestamps
+      t.text :description
+     	t.decimal :cost
+      t.integer :user_id
+      t.timestamps
     end
     add_index :courses, :user_id
   end
